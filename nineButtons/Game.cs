433db185621405c8480b2e8c217bef750565e2f7
SimplyY <845433424@qq.com>
2014-08-30 13:37:10 +0800

    finished design framework

---
+++ nineButtons/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nineButtons
+{
+    class Game
+    {
+        private enum name
+        {
+            player1,
+            player2,
+        }
+
+        public name currentPlayer;
+        
+
+        public void TansPlayer()
+        {
+            if (currentPlayer == player1)
+            {
+
+            }
+            
+        }
+
+    }
+}
